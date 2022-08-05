831e11325343f61871e2024a04e5fac8f9be0625
Min <biffboffkev@gmail.com>
2022-08-05 13:53:30 -0400

    commit tree d7fc495fe2dccb75d8c9ddfef4efbf1e957060ce

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random()+1);
+
+let num3=24
+let num4=5;
+let mod = num3%num4;
+
+
+let max = Math.max(4,1,6,4,2,3,6,20);
